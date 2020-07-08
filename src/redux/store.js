f123f4da7e7f8e32ea5d61db29f2e5be5460ef76
LordAldi <aldianugra09@gmail.com>
2020-07-09 00:01:02 +0700

    firebase deploy

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import {
     createStore,
     combineReducers,
-    applyMiddleware,
-    compose
+    applyMiddleware
 } from 'redux'
 import thunk from 'redux-thunk'
 import userReducer from './reducers/userReducer'
 import dataReducer from './reducers/dataReducer'
 import uiReducer from './reducers/uiReducer'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const initialState = {}
 
@@ -19,8 +19,7 @@ const reducers = combineReducers({
     UI: uiReducer
 })
 
-const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware),
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(...middleware)
     )
  )
 
